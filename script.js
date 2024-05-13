4d339d430fc0d6b0c79b93d34e99614a7a643743
jerrymarshall2004 <jerrymarshall2004@gmail.com>
2024-05-13 11:11:00 -0400

    speed improvment

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
+xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         text = (xhttp.responseText);
         parser = new DOMParser();
-        xmlDoc = parser.parseFromString(text,"text/xml");
+        xmlDoc = parser.parseFromString(text, "text/xml");
         titles = xmlDoc.getElementsByTagName("title");
         videoIDs = xmlDoc.getElementsByTagName("videoID");
         videoCount = xmlDoc.getElementsByTagName("title").length;
 
-        for (i = 0; i < videoCount; i++){
+        for (i = 0; i < videoCount; i++) {
             let img = new Image();
             let a = document.createElement('a');
             a.target = "_blank";
-            a.className="thumbnailLink"
+            a.className = "thumbnailLink"
             a.title = titles[i].childNodes[0].nodeValue;
-            a.href=("https://www.youtube.com/watch?v=" + videoIDs[i].childNodes[0].nodeValue);
-            img.className="thumbnail";
-            img.src ='images/thumbnails/' + videoIDs[i].childNodes[0].nodeValue + '.jpeg';
+            a.href = ("https://www.youtube.com/watch?v=" + videoIDs[i].childNodes[0].nodeValue);
+            img.className = "thumbnail";
+            img.src = 'images/thumbnails/' + videoIDs[i].childNodes[0].nodeValue + '.webp';
             a.appendChild(img);
 
             document.getElementById("workDiv").appendChild(a);
         }
-        
+
     }
 };
 xhttp.open("GET", "gallery.xml", true);
@@ -49,23 +49,23 @@ infoText = [
     Tripod:\
     <br>\
     SmallRig Freeblazer",
-    "<ul><li>H Visual Media</li><li>Ontario Tech Ridgebacks</li><li>Parkbridge</li><li>Duntroon Highlands Resort</li><li>Main Street Meaford</li><li>Rally Beer</li><li>Market Logics</li><li>South Georgian Bay Tourism</li><li>Whatz What</li></ul>"
+    "<ul><li>Nicky Romero</li><li>H Visual Media</li><li>Ontario Tech Ridgebacks</li><li>Parkbridge</li><li>Duntroon Highlands Resort</li><li>Main Street Meaford</li><li>Rally Beer</li><li>Market Logics</li><li>South Georgian Bay Tourism</li><li>Whatz What</li></ul>"
 ]
 
 var selectedButton;
 
-function textSelect(text){
+function textSelect(text) {
     textWrapper = document.getElementById('infoTextWrapper');
     infoTextP = document.getElementById('infoText');
     infoGrid = document.getElementById('infoGrid');
     selectedButton.classList.add('selection');
-    selectedButton = document.getElementById('selection'+text);
+    selectedButton = document.getElementById('selection' + text);
     selectedButton.classList.remove('selection');
-    infoTextP.innerHTML=infoText[text];
+    infoTextP.innerHTML = infoText[text];
     textWrapper.style.height = infoTextP.clientHeight + 20 + 'px';
 }
 
-function onload(){
+function onload() {
     selectedButton = document.getElementById('selection0');
     textSelect(0);
 }
